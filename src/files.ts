17106f52c3cfb8530dc5ad3c5c61c26a01760c17
Alejandro Hernández <info@alejandrohdezma.com>
2020-05-14 16:45:29 +0200

    Run `npm run format`

--- src/files.ts
+++ src/files.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
-import * as io from "@actions/io";
-import fs from "fs";
-import * as exec from "@actions/exec";
+import * as io from '@actions/io'
+import fs from 'fs'
+import * as exec from '@actions/exec'
 
 /**
  * Prepares the Scala Steward workspace that will be used when launching the app.
@@ -15,14 +15,16 @@ import * as exec from "@actions/exec";
  * @param repository The repository to update
  * @param token The Github Token used to authenticate into Github
  */
-export async function prepareScalaStewardWorkspace(repository: string, token: string): Promise<void> {
+export async function prepareScalaStewardWorkspace(
+  repository: string,
+  token: string
+): Promise<void> {
   try {
     await io.mkdirP('/opt/scala-steward')
     fs.writeFileSync('/opt/scala-steward/repos.md', `- ${repository}`)
     fs.writeFileSync('/opt/scala-steward/askpass.sh', `#!/bin/sh\n\necho '${token}'`)
     await exec.exec('chmod', ['+x', '/opt/scala-steward/askpass.sh'], {silent: true})
-  }
-  catch (error) {
+  } catch (error) {
     core.debug(error.message)
     throw new Error('Unable to create Scala Steward workspace')
   }
