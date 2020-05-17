a31da39f0f53a158c48c1ab6855974aa70f07ec8
Alejandro Hernández <info@alejandrohdezma.com>
2020-05-17 08:28:12 +0200

    Allow receiving a `Buffer` to write as `repos.md` instead of a name
    This `Buffer` will contain a repository list in Markdown format.

--- src/files.ts
+++ src/files.ts
@@ -7,21 +7,28 @@ import * as exec from '@actions/exec'
  * Prepares the Scala Steward workspace that will be used when launching the app.
  *
  * This will involve:
- * - Creating a folder `/ops/scala-steward`
- * - Creating a `repos.md` file inside workspace containing the repository to update
+ * - Creating a folder `/ops/scala-steward`.
+ * - Creating a `repos.md` file inside workspace containing the repository/repositories to update.
  * - Creating a `askpass.sh` file inside workspace containing the Github token.
  * - Making the previous file executable.
  *
- * @param {string} repository - The repository to update.
+ * @param {string | Buffer} repository - The repository to update or a file containing a list of
+ *                                       repositories in Markdown format.
  * @param {string} token - The Github Token used to authenticate into Github.
  */
 export async function prepareScalaStewardWorkspace(
-  repository: string,
+  repository: Buffer | string,
   token: string
 ): Promise<void> {
   try {
     await io.mkdirP('/opt/scala-steward')
-    fs.writeFileSync('/opt/scala-steward/repos.md', `- ${repository}`)
+
+    if (typeof repository === 'string') {
+      fs.writeFileSync('/opt/scala-steward/repos.md', `- ${repository}`)
+    } else {
+      fs.writeFileSync('/opt/scala-steward/repos.md', repository)
+    }
+
     fs.writeFileSync('/opt/scala-steward/askpass.sh', `#!/bin/sh\n\necho '${token}'`)
     await exec.exec('chmod', ['+x', '/opt/scala-steward/askpass.sh'], {silent: true})
   } catch (error) {
