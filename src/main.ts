1806398e6ee27e31d7a6a6ae2626a7d0da15be60
Alejandro Hernández <info@alejandrohdezma.com>
2022-12-06 08:55:22 +0100

    Improve how Scala Steward arguments are set

--- src/main.ts
+++ src/main.ts
@@ -48,25 +48,26 @@ async function run(): Promise<void> {
     }
 
     await coursier.launch('scala-steward', inputs.steward.version, [
-      ['--workspace', `${workspaceDir}/workspace`],
-      ['--repos-file', `${workspaceDir}/repos.md`],
-      ['--git-ask-pass', `${workspaceDir}/askpass.sh`],
-      ['--git-author-email', inputs.commits.author.email || user.email()],
-      ['--git-author-name', inputs.commits.author.name || user.name()],
-      ['--vcs-login', `${user.login()}"`],
-      ['--env-var', '"SBT_OPTS=-Xmx2048m -Xss8m -XX:MaxMetaspaceSize=512m"'],
-      ['--process-timeout', inputs.steward.timeout],
-      ['--vcs-api-host', inputs.github.apiUrl],
-      inputs.steward.ignoreOptsFiles ? '--ignore-opts-files' : [],
-      inputs.commits.sign.enabled ? '--sign-commits' : [],
-      inputs.commits.sign.key ? ['--git-author-signing-key', inputs.commits.sign.key] : [],
-      ['--cache-ttl', inputs.steward.cacheTtl],
-      inputs.migrations.scalafix ? ['--scalafix-migrations', inputs.migrations.scalafix] : [],
-      inputs.migrations.artifacts ? ['--artifact-migrations', inputs.migrations.artifacts] : [],
-      inputs.steward.defaultConfiguration ? ['--repo-config', inputs.steward.defaultConfiguration] : [],
+      arg('--workspace', `${workspaceDir}/workspace`),
+      arg('--repos-file', `${workspaceDir}/repos.md`),
+      arg('--git-ask-pass', `${workspaceDir}/askpass.sh`),
+      arg('--git-author-email', inputs.commits.author.email || user.email()),
+      arg('--git-author-name', inputs.commits.author.name || user.name()),
+      arg('--vcs-login', `${user.login()}"`),
+      arg('--env-var', '"SBT_OPTS=-Xmx2048m -Xss8m -XX:MaxMetaspaceSize=512m"'),
+      arg('--process-timeout', inputs.steward.timeout),
+      arg('--vcs-api-host', inputs.github.apiUrl),
+      arg('--ignore-opts-files', inputs.steward.ignoreOptsFiles),
+      arg('--sign-commits', inputs.commits.sign.enabled),
+      arg('--git-author-signing-key', inputs.commits.sign.key),
+      arg('--cache-ttl', inputs.steward.cacheTtl),
+      arg('--scalafix-migrations', inputs.migrations.scalafix),
+      arg('--artifact-migrations', inputs.migrations.artifacts),
+      arg('--repo-config', inputs.steward.defaultConfiguration),
+      arg('--github-app-id', inputs.github.app?.id),
+      arg('--github-app-key-file', inputs.github.app ? `${workspaceDir}/app.pem` : undefined),
       '--do-not-fork',
       '--disable-sandbox',
-      inputs.github.app ? ['--github-app-id', inputs.github.app.id, '--github-app-key-file', `${workspaceDir}/app.pem`] : [],
       inputs.steward.extraArgs ? inputs.steward.extraArgs.split(' ') : [],
     ]).finally(() => {
       workspace.saveWorkspaceCache(workspaceDir).catch((error: unknown) => {
@@ -75,6 +76,21 @@ async function run(): Promise<void> {
     })
   } catch (error: unknown) {
     core.setFailed(` ✕ ${(error as Error).message}`)
+  }
+}
+
+/**
+ * Creates an optional argument depending on an input's value.
+ *
+ * @param name Name of the arg being added.
+ * @param value The argument's value, empty string, false booleans or undefined will be skipped.
+ * @returns the argument to add if it should be added; otherwise returns `[]`.
+ */
+function arg(name: string, value: string | boolean | undefined) {
+  switch (typeof value) {
+    case 'string': { return (value === '') ? [] : [name, value] }
+    case 'boolean': { return value ? [name] : [] }
+    default: { return [] }
   }
 }
 
