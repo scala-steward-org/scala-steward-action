825ed48e5a30de77408b7786936df0b8d982f04d
Alejandro Hernández <info@alejandrohdezma.com>
2022-12-06 10:21:55 +0100

    Add method for creating a mandatory `NonEmptyString` (throws if empty)

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,22 @@ export class NonEmptyString {
   private constructor(readonly value: string) {}
 }
 
+/**
+ * Creates a `NonEmptyString` from a `string`. Returns `undefined` if the string is emtpy.
+ */
 export function nonEmpty(string: string): NonEmptyString | undefined {
   return NonEmptyString.from(string)
+}
+
+/**
+ * Creates a `NonEmptyString` from a `string`. Throws an `Error` if the string is empty.
+ */
+export function mandatory(string: string): NonEmptyString {
+  const value = nonEmpty(string)
+
+  if (value === undefined) {
+    throw new Error(`Input ${string} cannot be empty`)
+  }
+
+  return value
 }
