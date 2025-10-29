8b249748c2a6c945dd9f8179dd0e8e9f4704991f
Evgeniy Tokarev <archjohn@gmail.com>
2025-10-29 14:25:14 +0200

    code style improvements

--- src/modules/mill.ts
+++ src/modules/mill.ts
@@ -62,55 +62,51 @@ function getDownloadUrl(millVersion: string): string {
      || /^0\.2\.\d+$/.test(millVersion)
      || /^0\.3\.\d+$/.test(millVersion)
      || /^0\.4\.\d+$/.test(millVersion)) {
-      downloadSuffix = ""
-      downloadFromMaven = false
-  }
-  else if (/^0\.5\.\d+$/.test(millVersion)
+    downloadSuffix = ''
+    downloadFromMaven = false
+  } else if (/^0\.5\.\d+$/.test(millVersion)
           || /^0\.6\.\d+$/.test(millVersion)
           || /^0\.7\.\d+$/.test(millVersion)
           || /^0\.8\.\d+$/.test(millVersion)
           || /^0\.9\.\d+$/.test(millVersion)
           || /^0\.10\.\d+$/.test(millVersion)
-          || /^0\.11\.0-M-[A-Za-z0-9]+$/.test(millVersion)
-
-      ) {
-      downloadSuffix = "-assembly"
-      downloadFromMaven = false
-  }
-  else {
-      downloadSuffix = "-assembly"
-      downloadFromMaven = true
-  }
-
-  if (millVersion === "0.12.0"
-     || millVersion === "0.12.1"
-     || millVersion === "0.12.2"
-     || millVersion === "0.12.3"
-     || millVersion === "0.12.4"
-     || millVersion === "0.12.5"
-     || millVersion === "0.12.6"
-     || millVersion === "0.12.7"
-     || millVersion === "0.12.8"
-     || millVersion === "0.12.9"
-     || millVersion === "0.12.10"
-     || millVersion === "0.12.11") {
-      downloadExtension = 'jar'
-  } else if (/^0\.12\.[A-Za-z0-9]+$/.test(millVersion)) { // 0.12.*
-      downloadExtension = 'exe'
-  } else if (/^0\.\d+\.\d+(-[A-Za-z0-9.-]+)?$/.test(millVersion)) { // 0.*
-      downloadExtension = 'jar'
+          || /^0\.11\.0-M-[A-Za-z\d]+$/.test(millVersion)) {
+    downloadSuffix = '-assembly'
+    downloadFromMaven = false
   } else {
-      downloadExtension = 'exe'
+    downloadSuffix = '-assembly'
+    downloadFromMaven = true
+  }
+
+  if (millVersion === '0.12.0'
+     || millVersion === '0.12.1'
+     || millVersion === '0.12.2'
+     || millVersion === '0.12.3'
+     || millVersion === '0.12.4'
+     || millVersion === '0.12.5'
+     || millVersion === '0.12.6'
+     || millVersion === '0.12.7'
+     || millVersion === '0.12.8'
+     || millVersion === '0.12.9'
+     || millVersion === '0.12.10'
+     || millVersion === '0.12.11') {
+    downloadExtension = 'jar'
+  } else if (/^0\.12\.[A-Za-z\d]+$/.test(millVersion)) { // 0.12.*
+    downloadExtension = 'exe'
+  } else if (/^0\.\d+\.\d+(-[A-Za-z\d.-]+)?$/.test(millVersion)) { // 0.*
+    downloadExtension = 'jar'
+  } else {
+    downloadExtension = 'exe'
   }
 
   if (downloadFromMaven) {
-      millUrl = `https://repo1.maven.org/maven2/com/lihaoyi/mill-dist${artifactSuffix}/${millVersion}/mill-dist${artifactSuffix}-${millVersion}.${downloadExtension}`
+    millUrl = `https://repo1.maven.org/maven2/com/lihaoyi/mill-dist${artifactSuffix}/${millVersion}/mill-dist${artifactSuffix}-${millVersion}.${downloadExtension}`
   } else {
-      const millVersionTag = millVersion.replace(/([^-]+)(-M[0-9]+)?(-.*)?/, '$1$2')
-      millUrl = `https://github.com/lihaoyi/mill/releases/download/${millVersionTag}/${millVersion}${downloadSuffix}`
+    const millVersionTag = millVersion.replace(/([^-]+)(-M\d+)?(-.*)?/, '$1$2')
+    millUrl = `https://github.com/lihaoyi/mill/releases/download/${millVersionTag}/${millVersion}${downloadSuffix}`
   }
 
-  return millUrl;
+  return millUrl
 }
 
 function getArtifactSuffix(): string {
